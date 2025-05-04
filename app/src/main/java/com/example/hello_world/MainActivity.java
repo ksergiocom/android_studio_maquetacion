6602f02b08e8e81119286c04ae4a3f921b30424d
sergio <sergio@ksergio.com>
2025-05-04 17:03:24 +0200

    Scaffolding

---
+++ app/src/main/java/com/example/hello_world/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.hello_world;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // TopBar
+        MaterialToolbar topAppBar = findViewById(R.id.topAppBar);
+        setSupportActionBar(topAppBar);
+
+        // Bottom Navigation
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        Fragment inicioFragment = new Inicio();
+        Fragment rutasFragment = new Rutas();
+
+        setCurrentFragment(inicioFragment);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.nav_inicio) {
+                setCurrentFragment(inicioFragment);
+                return true;
+            } else if (id == R.id.nav_rutas) {
+                setCurrentFragment(rutasFragment);
+                return true;
+            }
+            return false;
+        });
+
+    }
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_app_bar_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Fragment configuracionFragment = new Configuracion();
+
+        if (item.getItemId() == R.id.nav_other) {
+            setCurrentFragment(configuracionFragment);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void setCurrentFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.flFragment, fragment)
+                .commit();
+    }
+
+}
